4c6faf34248e3679140b800361c24e4db2df842a
Kroganman <31574752+Kroganman@users.noreply.github.com>
2018-04-20 23:21:49 +0300

    Create rectangle.h

---
+++ Practical works/prac7/rectangle.h
@@ -0,0 +1,11 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+#include "quadrilateral.h"
+
+class Rectangle : public Quadrilateral
+{
+public:
+    Rectangle(double a,double b);
+};
+
+#endif // RECTANGLE_H
